0ff38b7d83cc8af98121d73ca75dc194544619eb
Robert P <robert.popovici57@gmail.com>
2024-11-27 23:20:40 +0200

    Lab 7 2

--- Lab 7/ass1.m
+++ Lab 7/ass1.m
@@ -1,7 +1,6 @@
-fs = 44100;  
-center_freqs = [31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]; 
-
-
+fs = 44100;
+center_freqs = [31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000];
+Q = 1.446;
 
 figure;
 subplot(2,1,1);
@@ -10,31 +9,37 @@ title('Frequency response of individual filters');
 xlabel('Normalized Frequency');
 ylabel('Magnitude');
 
-
-sum_response = zeros(1024, 1); % 
+sum_response = zeros(1024, 1);
 
 
 for i = 1:length(center_freqs)
- 
-    fc1 = center_freqs(i) / sqrt(2); 
-    fc2 = center_freqs(i) * sqrt(2);  
-    
-    if fc2 > fs / 2
-        fc2 = fs / 2 - 1;
-    end
+    if(i==1)
+        fc1 = center_freqs(i) *(1/(2*Q) + sqrt(1 + 1/(4*Q^2)));
+        b=fir1(50,fc1/(fs/2),'low');
+        [H, f] = freqz(b, 1, 1024, fs);
+        plot(f / fs, abs(H));
+        sum_response = sum_response + abs(H);
+    else if(i == length(center_freqs))
+            fc1 = center_freqs(i) *(-1/(2*Q) + sqrt(1 + 1/(4*Q^2)));
+            b=fir1(50,fc1/(fs/2),'high');
+            [H, f] = freqz(b, 1, 1024, fs);
+            plot(f / fs, abs(H));
+            sum_response = sum_response + abs(H);
+    else
+        fc1 = center_freqs(i) *(-1/(2*Q) + sqrt(1 + 1/(4*Q^2)));
+        fc2 = center_freqs(i) *(1/(2*Q) + sqrt(1 + 1/(4*Q^2)));
 
-    b = fir1(50, [fc1, fc2] / (fs/2), 'bandpass');    
-    [H, f] = freqz(b, 1, 1024, fs); 
-    plot(f / fs, abs(H));      
-    sum_response = sum_response + abs(H);
+       
+        b = fir1(50,[fc1,fc2]/ (fs/2), 'bandpass');
+        [H, w] = freqz(b, 1, 1024, fs);
+        plot(f / fs, abs(H));
+        sum_response = sum_response + abs(H);
+    end
+    end
 end
 
-
-
-
-
 subplot(2,1,2);
-plot(f / fs, 20*log10(sum_response)); 
+plot(f / fs, 20*log10(sum_response));
 title('Frequency response of sum of filters');
 xlabel('Normalized Frequency');
 ylabel('20*log10(Magnitude)');
